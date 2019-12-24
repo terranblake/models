db95de43d9ca0bbbc213148f1a2e0791dada57f4
Terran Blake <terranblake@gmail.com>
2019-12-23 20:56:34 -0800

    remove extra step and export each model by name

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 "use strict";
+// classes
+// import { default as CompanyClass } from './classes/Company';
+// import { default as FactClass } from './classes/Fact';
+// import { default as FactDateClass } from './classes/FactDate';
+// import { default as FilingClass } from './classes/Filing';
+// import { default as FilingDocumentClass } from './classes/FilingDocument';
+// import { default as IdentifierClass } from './classes/Identifier';
+// import { default as LinkClass } from './classes/Link';
 exports.__esModule = true;
-var index_1 = require("./src/index");
-exports["default"] = index_1["default"];
+// const classes = {
+// 	Company: CompanyClass,
+// 	Fact: FactClass,
+// 	FactDate: FactDateClass,
+// 	Filing: FilingClass,
+// 	FilingDocument: FilingDocumentClass,
+// 	Identifier: IdentifierClass,
+// 	Link: LinkClass
+// }
+// models
+var Company_1 = require("./src/models/Company");
+var Fact_1 = require("./src/models/Fact");
+var Filing_1 = require("./src/models/Filing");
+var FilingDocument_1 = require("./src/models/FilingDocument");
+var Identifier_1 = require("./src/models/Identifier");
+var Link_1 = require("./src/models/Link");
+exports.models = {
+    Company: Company_1["default"],
+    Fact: Fact_1["default"],
+    Filing: Filing_1["default"],
+    FilingDocument: FilingDocument_1["default"],
+    Identifier: Identifier_1["default"],
+    Link: Link_1["default"]
+};
+// todo: add support for typegoose?
--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-export { models as default } from './src/index';
+// classes
+// import { default as CompanyClass } from './classes/Company';
+// import { default as FactClass } from './classes/Fact';
+// import { default as FactDateClass } from './classes/FactDate';
+// import { default as FilingClass } from './classes/Filing';
+// import { default as FilingDocumentClass } from './classes/FilingDocument';
+// import { default as IdentifierClass } from './classes/Identifier';
+// import { default as LinkClass } from './classes/Link';
+
+// const classes = {
+// 	Company: CompanyClass,
+// 	Fact: FactClass,
+// 	FactDate: FactDateClass,
+// 	Filing: FilingClass,
+// 	FilingDocument: FilingDocumentClass,
+// 	Identifier: IdentifierClass,
+// 	Link: LinkClass
+// }
+
+// models
+import { default as CompanyModel } from './src/models/Company';
+import { default as FactModel } from './src/models/Fact';
+import { default as FilingModel } from './src/models/Filing';
+import { default as FilingDocumentModel } from './src/models/FilingDocument';
+import { default as IdentifierModel } from './src/models/Identifier';
+import { default as LinkModel } from './src/models/Link';
+
+export const models = {
+	Company: CompanyModel,
+	Fact: FactModel,
+	Filing: FilingModel,
+	FilingDocument: FilingDocumentModel,
+	Identifier: IdentifierModel,
+	Link: LinkModel
+}
+
+// todo: add support for typegoose?
